153dd0e56612c34385b816a1d2e6a4905075c5db
aVishalv <126471933+aVishalv@users.noreply.github.com>
2026-02-10 11:00:53 +0530

    changes

--- lib/app/screens/base_page/base_binding.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-class BasePageBinding implements Bindings {
-  @override
-  void dependencies() {
-    // Get.lazyPut<AppDrawerController>(() => AppDrawerController());
-  }
-}
