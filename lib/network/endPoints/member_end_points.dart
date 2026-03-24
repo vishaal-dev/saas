edc03ab40f77f9daccf11ff20ca022c80e3a5957
aVishalv <126471933+aVishalv@users.noreply.github.com>
2026-03-24 18:26:07 +0530

    logo changes and behaviour

--- lib/network/endPoints/member_end_points.dart
+++
@@ -1,7 +0,0 @@
-/// Member asset paths (relative to [ApiEndPoints.baseUrl]).
-abstract final class MemberEndPoints {
-  MemberEndPoints._();
-
-  static const String schemaMember = '/schema/asset/member';
-  static const String contentMember = '/content/asset/member';
-}
--- lib/network/services/services.dart
+++
@@ -1,2 +0,0 @@
-export 'api_end_points.dart';
-export 'api_services.dart';
